1a5c1acf15c5ca4724a555b0576b305560da55e8
Oscar Piriz <owcpirez@gmail.com>
2021-11-08 16:48:02 -0300

    Creacion de usuarios

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import redirect
 
 from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.contrib.auth import authenticate
+
+from django.contrib.auth.models import User
+
+from .forms import RegisterForm
 
 def index(request):
     return render(request, 'index.html', {
@@ -31,4 +36,27 @@ def login_view(request):
 
     return render(request, 'users/login.html', {
 
+    })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesión cerrada exitosamente')
+    return redirect('login')
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        username = form.cleaned_data.get('username') #Dicionario
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+        user = User.objects.create_user(username, email, password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('index')
+
+    return render(request, 'users/register.html', {
+        'form': form
     })
