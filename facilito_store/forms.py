2cfd7e1f53fba012ca8ed6099ead2d911d5bbfd5
Oscar Piriz <owcpirez@gmail.com>
2021-11-09 12:05:18 -0300

    Facilito Store

--- facilito_store/forms.py
+++ facilito_store/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
     username = forms.CharField(required=True,
@@ -19,3 +20,18 @@ class RegisterForm(forms.Form):
                                    'class': 'form-control',
                                    'id': 'password'
                                }))
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El username ya se encuentra en uso')
+
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya se encuentra en uso')
+
+        return email
